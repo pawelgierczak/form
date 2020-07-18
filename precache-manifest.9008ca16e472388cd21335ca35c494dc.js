49c291c45b2544d4ccbfd97ebd5ac2e950124fe9
pawelgierczak <cabalonline2002@gmail.com>
2020-07-18 23:01:47 +0200

    Updates

---
+++ precache-manifest.9008ca16e472388cd21335ca35c494dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd93331a42f0aa0badb1c8687b64d091",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "7fdb019beda9c4a67ca8",
+    "url": "/form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4a58de4b3553903a2e7e",
+    "url": "/form/static/js/2.48c8b392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form/static/js/2.48c8b392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdb019beda9c4a67ca8",
+    "url": "/form/static/js/main.b895340a.chunk.js"
+  },
+  {
+    "revision": "e06a19fd52a8db971a6b",
+    "url": "/form/static/js/runtime-main.3106db36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/form/static/media/logo.5d5d9eef.svg"
+  }
+]);
